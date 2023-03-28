cabc35bab4f4c93900de227aa0e71344f163bc88
Boivinos <boivinantonin@gmail.com>
2023-03-28 11:02:22 +0200

    created mew with ??? instead of img

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,30 @@
-
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 function PokemonCard() {
-    return (
+    const pokemon = pokemonList[0];
+    const src = pokemon.imgSrc
+    return src != undefined ?
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-            <figcaption>Bulbizar il est trop bizare</figcaption>
+            <img src={pokemon.imgSrc} />
+            <figcaption>Le pokemon est {pokemon.name}</figcaption>
+        </figure> :
+        <figure>
+            <p>???</p>
+            <figcaption>Le pokemon est {pokemon.name}</figcaption>
         </figure>
-    );
 
 }
+
+
+
+
 export default PokemonCard;

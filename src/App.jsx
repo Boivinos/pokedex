0fd56b185923e6bc4767bcd1a2f8fc2ed12e39e8
Boivinos <boivinantonin@gmail.com>
2023-03-28 14:09:43 +0200

    used state to create buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
-import MyTitle from "./components/MyTitle"
-import PokemonCard from "./components/PokemonCard"
 
+import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
+import "./App.css"
 
 
 function App() {
+
   const pokemonList = [
     {
       name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
-    { name: "mew", },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
   ];
 
-  return (
-    <div>
-      <PokemonCard pokemon={pokemonList[0]} />
-    </div>
+  const [pokemonInd, setPokemon] = useState(0);
 
-  );
+  const handleClickSuiv = () => {
+    setPokemon(pokemonInd + 1)
+
+  }
+  const handleClickPrec = () => {
+    setPokemon(pokemonInd - 1)
+
+  }
+
+  return pokemonInd === 0 ?
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonInd]} />
+      <button onClick={handleClickSuiv}>Prochain pokemon</button>
+    </div> : pokemonInd === pokemonList.length - 1 ?
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonInd]} />
+        <button onClick={handleClickPrec}>Précédent pokemon</button>
+      </div> :
+      <div>
+        <PokemonCard pokemon={pokemonList[pokemonInd]} />
+        <button onClick={handleClickPrec}>Précédent pokemon</button>
+        <button onClick={handleClickSuiv}>Prochain pokemon</button>
+      </div>
+
+
+    ;
 }
 
 export default App;
